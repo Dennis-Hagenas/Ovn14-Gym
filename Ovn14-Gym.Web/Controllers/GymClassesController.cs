f368ca5b659f618669980faf202d4a97b9576fab
Dennis Hagenäs <dh_lexicon@outlook.com>
2023-01-27 16:37:35 +0100

    ApplicationFactory

--- Ovn14-Gym.Web/Controllers/GymClassesController.cs
+++ Ovn14-Gym.Web/Controllers/GymClassesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Ovn14_Gym.Core.Entities;
 using Ovn14_Gym.Data.Data;
 using Ovn14_Gym.Web.Data;
+using Ovn14_Gym.Web.Models;
 
 namespace Ovn14_Gym.Web.Controllers
 {
@@ -159,6 +161,13 @@ namespace Ovn14_Gym.Web.Controllers
         private bool GymClassExists(int id)
         {
           return (_context.GymClasses?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Ovn14-Gym.Web/Program.cs
+++ Ovn14-Gym.Web/Program.cs
@@ -8,8 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => {
@@ -33,7 +35,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/GymClasses/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -47,7 +49,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=GymClasses}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
