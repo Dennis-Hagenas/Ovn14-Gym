386a77b8d466206a7e17e60f29c3b0cdd4cc9fde
Dennis Hagenäs <dh_lexicon@outlook.com>
2023-01-31 16:38:24 +0100

    viewmodel, move interfaces

---
+++ Ovn14-Gym.Core/ViewModels/GymClassesViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovn14_Gym.Data.ViewModels
+{
+    public class GymClassesViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime? StartTime { get; set; }
+
+        [Required]
+        public TimeSpan? Duration { get; set; }
+        public DateTime? EndTime { get { return StartTime + Duration; } }
+        public string Description { get; set; } = string.Empty;
+    }
+}
